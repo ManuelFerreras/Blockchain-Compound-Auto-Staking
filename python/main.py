2379947fb1c9216b319abf39039e5e1bf89657e4
Manuel Ferreras <35044975+ManuelFerreras@users.noreply.github.com>
2021-12-10 18:47:19 -0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -2,17 +2,16 @@ import pyautogui
 import time
 
 while True:
-    try:
-        posEst = pyautogui.locateOnScreen('estimated.png')
-        if posEst != None:
-            pyautogui.click(posEst[0], posEst[1])
-            pyautogui.press('end')
-            time.sleep(0.4)
+    posEst = pyautogui.locateOnScreen('estimated.png')
+    if posEst != None:
+        pyautogui.click(posEst[0], posEst[1])
+        pyautogui.press('end')
+        time.sleep(0.4)
+        try:
             posAccept = pyautogui.locateOnScreen('confirm.png')
             pyautogui.click(posAccept[0]+10, posAccept[1]+10)
             print("pressed")
-    except:
-        print("Error")
-        time.sleep(1)
+        except:
+            pass
 
 # pip install pyautogui
